d1b9caf06daefd90c8efece2dc3e6f5ddc5bab61
Aashi <aashi@Aashis-MacBook-Air.local>
2026-02-28 17:14:24 +0530

    int and float practice

---
+++ calculator.py
@@ -0,0 +1,17 @@
+# x = input("Enter x: ")
+# y = input("Enter y: ")
+
+# z= int(x) + int(y)
+# print(z)
+
+# x = int(input("Enter x: "))
+# y = int(input("Enter y: "))
+
+# print(x+y)
+
+x = float(input("Enter x: "))
+y = float(input("Enter y: "))
+z = round(x+y)
+m = x/y
+print(f"{z}")
+print(f"{m:.2f}")

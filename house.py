9d99f99bb159549a6d978ef5edad0bda6d377e90
Amarjeet <amarjeet.111035@gmail.com>
2026-03-01 18:11:04 +0530

    switch condition in python

---
+++ house.py
@@ -0,0 +1,23 @@
+name = input("What's your name: ")
+
+# switch condition in python 3.10 and above
+match name:
+    case "Harry":
+        print("Gryffindor")
+    case "Hermione":
+        print("Gryffindor")
+    case "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _:
+        print("Who?")
+
+# more refined version for switch condition in python 3.10 and above
+match name:
+    case "Harry" | "Hermione" | "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    case _:
+        print("Who?")

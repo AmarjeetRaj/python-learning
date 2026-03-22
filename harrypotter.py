0f2d42fbb38be52e2510574c7b081df7671f7f28
Amarjeet <amarjeet.111035@gmail.com>
2026-03-22 14:58:29 +0530

    DictReader and DictWriter explained

--- harrypotter.py
+++ harrypotter.py
@@ -2,11 +2,10 @@ import csv
 students = []
 
 with open("harrypotter.csv") as file:
-    reader = csv.reader(file)
+    #reader = csv.reader(file)
+    reader = csv.DictReader(file)
     for row in reader:
-        name = row[0]
-        home = ", ".join(row[1:])
-        students.append({"name": name, "home": home})
+        students.append({"name": row["name"], "home": row["home"]})
 
 for student in sorted(students, key=lambda student: student["name"]):
     print(f"{student['name']} is from {student['home']}")
